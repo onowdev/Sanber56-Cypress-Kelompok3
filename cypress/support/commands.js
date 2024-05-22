2daf6ae09534227f16f3b41f19bc89a0260668ea
regitarahma18 <regitarahma18@gmail.com>
2024-05-22 09:30:28 +0700

    update myAccount

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,6 +26,43 @@ Cypress.Commands.add('login', (email, pass) => {
      cy.get("#pass").type(pass)
  })
 
+//MyAccount
+Cypress.Commands.add('navigateToAccountInfo', (option = 'Edit') => {
+    cy.visit("https://magento.softwaretestingboard.com/customer/account/");
+    cy.wait(2000);
+    cy.get('.customer-welcome', { timeout: 10000 }).first().click(); 
+    // cy.get('a[title="My Account"]', { timeout: 10000 }).click();
+
+    switch(option) {
+        case 'Edit':
+            cy.contains('Edit').click();
+            break;
+        case 'Change Password':
+            cy.contains('Change Password').click();
+            break;
+        case 'Edit Default Billing Address':
+            cy.contains('Edit Address').eq(0).click(); // Memilih elemen pertama untuk default billing address
+            break;
+        case 'Edit Default Shipping Address':
+            cy.contains('Edit Address').eq(1).click(); // Memilih elemen kedua untuk default shipping address
+            break;
+        default:
+            throw new Error(`Unknown option: ${option}`);
+    }
+});
+
+// Cypress.Commands.add('navigateToAccountInfo', () => {
+//     cy.get('.customer-welcome').first().click();
+//     cy.get('a[title="Edit"]').click();
+//     cy.get('a:contains("Account Information")').click();
+// });
+
+// Cypress.Commands.add('navigateToAddress', () => {
+//     cy.get('.customer-welcome').click();
+//     cy.get('a[title="My Account"]').click();
+//     cy.get('a:contains("Address Book")').click();
+// });
+
 
 //
 // -- This is a child command --
