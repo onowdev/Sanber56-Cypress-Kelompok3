4a74d27de95daccd5c77b37c1b1169219eba8c45
regitarahma18 <regitarahma18@gmail.com>
2024-05-22 09:30:56 +0700

    myAccountInfo

---
+++ cypress/support/pageObject/myAccountInfo.js
@@ -0,0 +1,35 @@
+// cypress/page_objects/AccountInfoPage.js
+
+class AccountInfoPage {
+    visit() {
+        cy.navigateToAccountInfo();
+    }
+
+    fillForm(firstName, lastName) {
+        cy.get('#firstname').clear().type(firstName);
+        cy.get('#lastname').clear().type(lastName);
+        cy.get('button[title="Save"]').click();
+    }
+
+    clearForm() {
+        cy.get('#firstName').clear();
+        cy.get('#lastName').clear();
+    }
+
+    changeEmail(newEmail, currentPassword) {
+        cy.get('#change-email').click();
+        cy.get('#email').clear().type(newEmail);
+        cy.get('#current-password').type(currentPassword);
+        cy.get('button[title="Save"]').click();
+    }
+
+    changePassword(currentPassword, newPassword, confirmPassword) {
+        cy.get('#change-password').click();
+        cy.get('#current-password').type(currentPassword);
+        cy.get('#password').type(newPassword);
+        cy.get('#password-confirmation').type(confirmPassword);
+        cy.get('button[title="Save"]').click();
+    }
+}
+
+export default AccountInfoPage;
